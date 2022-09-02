f098ab32e0300d74a2ec6a5759425cd227dab6e4
xorgal <93151521+xorgal@users.noreply.github.com>
2022-09-02 06:27:06 +0300

    clear unused imports

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,9 +1,7 @@
 import logo from "./logo.svg";
 import menu from "./menu.svg";
 import close from "./close.svg";
-import google from "./google.svg";
-import apple from "./apple.svg";
 import twitter from "./twitter.svg";
 import wallet from "./wallet.png";
 
-export { logo, menu, close, google, apple, twitter, wallet };
+export { logo, menu, close, twitter, wallet };

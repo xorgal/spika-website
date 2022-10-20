da1a3630668d70bd515fa17a2e878a45bf12a294
xorgal <93151521+xorgal@users.noreply.github.com>
2022-10-20 08:54:51 +0300

    Add svg logo export

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,8 +1,9 @@
 import logo from "./logo.svg";
 import logo400 from "./logo_400_nb.png";
+import logoSvg from "./logo_400_svg.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
 import twitter from "./twitter.svg";
 import wallet from "./wallet.png";
 
-export { logo, logo400, menu, close, twitter, wallet };
+export { logo, logo400, logoSvg, menu, close, twitter, wallet };
